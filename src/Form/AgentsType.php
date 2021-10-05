f118d6e923599b069bb030990a787c4d2a420a77
Bruno Richard <bruno683@outlook.fr>
2021-10-05 11:18:04 +0200

    Add agentsCrud

---
+++ src/Form/AgentsType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Agents;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AgentsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('codeName')
+            ->add('skills')
+            ->add('dateOfBirth')
+            ->add('nationality')
+            ->add('missions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Agents::class,
+        ]);
+    }
+}
