e03bc430b6b8061612eea8ff48e5ceacc9f5bdc6
Bruno Richard <bruno683@outlook.fr>
2021-10-06 11:49:27 +0200

    Add login form

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -50,9 +50,9 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+       
+        return new RedirectResponse($this->urlGenerator->generate('missions_index'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
